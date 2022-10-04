d873b141afb1482df584d766932f5291d5eb3046
MolleMathias <mollemathiasing@gmail.com>
2022-10-04 02:25:34 +0200

    Portafolio 2

---
+++ ProyectoClases/views.py
@@ -0,0 +1,70 @@
+from re import template
+from xmlrpc.client import NOT_WELLFORMED_ERROR
+from django.http import HttpResponse
+from datetime import datetime
+from django.template import Context, Template, loader   #para generar template
+import random
+from home.models import Persona
+
+def hola (request):
+    return HttpResponse("Hola a")
+
+def fecha (request):
+    fecha_y_hora = datetime.now()
+    return HttpResponse(f"La fecha y hora actual es {fecha_y_hora}")
+
+def fecha_nacimiento (request, edad):
+    fecha = datetime.now().year - edad
+    return HttpResponse(f'La fecha aproximada een q nacio persona es {fecha}')
+
+def mi_template (request):
+    cargar_archivo = open(r'C:\Users\Mathias\Documents\Python - Coderhouse\Proyecto-clases\Templates\mi_template.html', 'r')  #llamamos el archivo al cual le queremos hacer un template
+    template = Template(cargar_archivo.read())                                                                             #con esto generamos un template del archivo q teniamos enn html
+    cargar_archivo.close()                                                                                                 #cerramos el archivo
+    contexto = Context()                                                                                                   #creamos un contexto
+    template_renderizado = template.render(contexto)                                                                       #puesta en marcha, dibujamos el template 
+    return HttpResponse(template_renderizado)                                                                              #pasamos el template al http response
+
+def tu_template (request, nombre):
+    # cargar_archivo = open(r'C:\Users\Mathias\Documents\Python - Coderhouse\Proyecto-clases\Templates\tu_template.html', 'r')  #llamamos el archivo al cual le queremos hacer un template
+    # template = Template(cargar_archivo.read())                                                                             #con esto generamos un template del archivo q teniamos enn html
+    # cargar_archivo.close()                                                                             #cerramos el archivo
+    # contexto = Context({'persona': nombre})                                                                                #creamos un contexto, tiene la inforkación q le queremos pasar al templateS
+    # template_renderizado= template.render(contexto)                                                                       #puesta en marcha, dibujamos el template 
+    
+    #forma simplificada:
+    template = loader.get_template('tu_template.html')    #La direccion de la carpeta no la puse completa porq la asigne en el archivo settings en DIR
+    template_renderizado = template.render({'persona': nombre})
+    
+    return HttpResponse(template_renderizado)   
+
+def prueba_template (request):
+    
+    mi_contexto = {'rango' : list(range(1,11)),
+                   'valor_aleatorio': random.randrange(1,11),
+                   }
+    
+    
+    template = loader.get_template('prueba_template.html')    #La direccion de la carpeta no la puse completa porq la asigne en el archivo settings en DIR
+    template_renderizado = template.render(mi_contexto)
+    
+    return HttpResponse(template_renderizado) 
+
+def crear_persona(request, nombre, apellido):
+    
+    persona = Persona(nombre=nombre, apellido=apellido, edad= random.randrange(1,99), fecha_nacimiento=datetime.now())
+    persona.save()
+    template = loader.get_template('crear_persona.html')    
+    template_renderizado = template.render({'persona': persona})
+    
+    return HttpResponse('')
+
+def ver_personas(request):
+    
+    #le paso a la base de datos todfos los objetos de personas
+    
+    persona = Persona.objects.all()
+    template = loader.get_template('ver_persona.html')    
+    template_renderizado = template.render({'personas': persona})
+    
+    return HttpResponse(template_renderizado)
