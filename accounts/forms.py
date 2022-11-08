ab6abe323ebf18d9dcb641e4766a9e1b389ecc45
MolleMathias <mollemathiasing@gmail.com>
2022-11-08 19:58:14 +0100

    Clase 24 completa

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,26 @@
 from dataclasses import fields
-from django.contrib.auth.forms import UserChangeForm
+import email
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
 from django import forms       
 
 #vamos a crear nuestro propio formulario usando como base el de django
 
-class MiFormularioDeCreacion(UserChangeForm):
+class MiFormularioDeCreacion(UserCreationForm):
     
     email = forms.CharField(label= 'Email' , max_length=20)
-    password1 = forms.CharField(label= 'Contrasenia', widget=forms.PasswordInput)
-    password2 = forms.CharField(label= 'Repetir contrasenia', widget=forms.PasswordInput)
+    password1 = forms.CharField(label= 'Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label= 'Repetir contraseña', widget=forms.PasswordInput)
     
     class Meta:
         
         model = User
         fields = ['username', 'email', 'password1', 'password2' ]
         help_texts = {key: '' for key in fields}  
+        
+        
+class EditarPerfilFormulario(forms.Form):
+    email = forms.CharField()
+    first_name = forms.CharField(label= 'Nombre')
+    last_name = forms.CharField(label= 'Apellido')
+    avatar = forms.ImageField(required=False)
